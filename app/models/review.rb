7eeb90e32d052ac50e94e6c756ba11ebc3983dd1
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-05-28 17:04:43 +0100

    associated reviews to users

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ActiveRecord::Base
   belongs_to :restaurant
 
   validates :rating, inclusion: (1..5)
+  validates :user, uniqueness: { scope: :restaurant, message: 'has reviwed this restaurant already' }
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,5 +13,35 @@ feature 'reviewing' do
       expect(current_path).to eq'/restaurants'
       expect(page).to have_content ('meh')
     end
+context 'reviewing twice'do
+    scenario 'user has reviewed a restaurant already' do
+       before do
+      visit '/'
+      click_link('Sign up')
+      fill_in('Email', with: 'test@email.com')
+      fill_in('Password', with: 'password')
+      fill_in('Password confirmation', with: 'password')
+      click_button('Sign up')
+    end
+visit '/restaurants'
+      click_link 'Review KFC'
+      fill_in 'Thoughts', with: 'meh'
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+
+      expect(current_path).to eq'/restaurants'
+      expect(page).to have_content ('meh')
+      visit '/restaurants'
+      click_link 'Review KFC'
+      fill_in 'Thoughts', with: 'meh'
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+
+      expect(current_path).to eq'/restaurants'
+      expect(page).to have_content ('meh')
+
+    end
+
+  end
 
 end
